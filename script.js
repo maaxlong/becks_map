cef04802ebdfbfd69bdbd7a81135241f9c694746
maaxlong <157611280+maaxlong@users.noreply.github.com>
2024-10-30 11:13:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// Initialize the map
-var map = L.map('map').setView([latitude, longitude], zoomLevel);
-
 var latitude = (53.7808253 + 53.804090352) / 2;  // ~53.792457826
 var longitude = (-1.7708349 + -1.726866479) / 2; // ~-1.7488506895
 var zoomLevel = 15;
+
+// Initialize the map
+var map = L.map('map').setView([latitude, longitude], zoomLevel);
+
+
 
 var bounds = [
   [53.7808253, -1.7708349],      // Southwest corner

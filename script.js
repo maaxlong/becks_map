5bfa02c79cf505f95e8382399861559ab9606417
maaxlong <157611280+maaxlong@users.noreply.github.com>
2024-12-13 17:34:59 +0000

    Update script.js
    - zoom levels fixed
    - updated to three layers
    - greyed out areas of 1873 and 1908 layers that are not included in the calculations, as bounded by the limits of 1852 towns map

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Define latitude, longitude, and zoom level
   var latitude = (53.7808253 + 53.804090352) / 2;      // Approximately 53.792457826
   var longitude = (-1.7708349 + -1.726866479) / 2;     // Approximately -1.7488506895
-  var zoomLevel = 15; // Adjust as needed
+  var zoomLevel = 13; // Adjust as needed
 
   // Initialize the map
   var map = L.map('map').setView([latitude, longitude], zoomLevel);
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Set the maximum bounds of the map
   map.setMaxBounds(bounds);
-
+  
   // Create length control
   var lengthControl = L.control({ position: 'bottomright' });
 
@@ -51,6 +51,36 @@ document.addEventListener('DOMContentLoaded', function() {
       return container;
     }
   });
+
+  var visibleBounds = L.latLngBounds(
+    [53.7808253, -1.7708349],  // Southwest corner
+    [53.804090352, -1.726866479]  // Northeast corner
+  );
+
+  // Create a larger polygon covering the entire map area
+  var outerBounds = [
+    [-90, -180],
+    [90, -180],
+    [90, 180],
+    [-90, 180]
+  ];
+
+  // Convert bounds to a polygon for the visible area
+  var innerBounds = [
+    [53.7808253, -1.7708349],
+    [53.804090352, -1.7708349],
+    [53.804090352, -1.726866479],
+    [53.7808253, -1.726866479],
+    [53.7808253, -1.7708349]
+  ];
+
+  // Create a mask using the outer polygon with a hole for the visible area
+  var mask = L.polygon([outerBounds, innerBounds], {
+    color: 'black',
+    fillColor: 'black',
+    fillOpacity: 0.7, // Adjust opacity to block tiles
+    stroke: false
+  }).addTo(map);
 
   // Function to check if all layers are loaded
   function checkLayersLoaded() {
@@ -115,12 +145,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Define basemaps
   var basemap1 = L.tileLayer('https://mapseries-tilesets.s3.amazonaws.com/os/town-england/Bradford/{z}/{x}/{y}.png', {
+    bounds: bounds, 
+    minZoom: 13,
+  	maxZoom: 20,
     attribution: 'National Library of Scotland'
   }); // 1852
   var basemap2 = L.tileLayer('https://allmaps.xyz/maps/682c0c5a6a17c868/{z}/{x}/{y}.png', {
+    bounds: bounds, 
+    minZoom: 13,
+  	maxZoom: 20,
     attribution: 'National Library of Scotland'
   }); // 1873
   var basemap3 = L.tileLayer('https://mapseries-tilesets.s3.amazonaws.com/25_inch/yorkshire/{z}/{x}/{y}.png', {
+    bounds: bounds,
+    minZoom: 13,
+  	maxZoom: 20,
     attribution: 'National Library of Scotland'
   }); // 1908
 
